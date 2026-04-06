221eb76f2c87fcada980fead47a458121507e69f
MadelineColbert <madeline.r.colbert+eglrsaf4@gmail.com>
2026-04-06 13:45:41 -0500

    Direct assignment to struct

--- main.cu
+++ main.cu
@@ -3,11 +3,10 @@
 
 int main() {
   NN network;
-  int layers[] = {784,100,10};
+  int network.layers = {784,100,10};
   float *d_images, *d_labels;
   load_data(784, &d_images, &d_labels);
 
-  network.layers = layers;
   init_nn(&network);
 
   free_nn(&network);
