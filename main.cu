55d132beb68b7ca12dc16dbfecefbcf571b5f78e
MadelineColbert <madeline.r.colbert+eglrsaf4@gmail.com>
2026-04-06 13:47:56 -0500

    Manual layer init

--- main.cu
+++ main.cu
@@ -3,7 +3,9 @@
 
 int main() {
   NN network;
-  network.layers = {784,100,10};
+  network.layers[0] = 784;
+  network.layers[1] = 100
+  network.layers[2] = 10;
   float *d_images, *d_labels;
   load_data(784, &d_images, &d_labels);
 
